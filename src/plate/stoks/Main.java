c56b706d3e1f3a38839d2333da282472ec1a8dfb
HizZapps <hizz.apps@gmail.com>
2024-07-10 22:44:11 +0300

    10.07.24

--- src/plate/stoks/Main.java
+++ src/plate/stoks/Main.java
@@ -1,8 +1,6 @@
 package plate.stoks;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -19,7 +17,6 @@ public class Main {
         storageList.put(600, 100);
         storageList.put(601, 600);
         storageList.put(301, 600);
-        storageList.put(202, 600);
 
         int widthDet = 100; //Ширина деталі
         int lengthDet = 200; //Довжина довжина
