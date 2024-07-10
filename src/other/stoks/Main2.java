02c81fe198ccd725d18f38f65ddea6ef12e6e1d3
HizZapps <hizz.apps@gmail.com>
2024-07-10 22:52:38 +0300

    10.07.24

--- src/other/stoks/Main2.java
+++ src/other/stoks/Main2.java
@@ -1,57 +1,36 @@
 package other.stoks;
 
-        import java.util.HashMap;
-        import java.util.Map;
+import java.util.ArrayList;
 
 public class Main2 {
     public static void main (String[] args){
 
-        Map<Integer, Integer> storageList = new HashMap<>();
+        ArrayList<Integer> aList = new ArrayList<>();
+        //Додаємо заготовки на склад
+        aList.add(70);
+        aList.add(350);
+        aList.add(50);
+        aList.add(148);
+        aList.add(449);
+        aList.add(899);
 
-        //Додаємо габарити заготовок на склад (ширина, довжина)
-        storageList.put(100, 100);
-        storageList.put(200, 100);
-        storageList.put(201, 200);
-        storageList.put(300, 300);
-        storageList.put(101, 600);
-        storageList.put(600, 100);
-        storageList.put(601, 600);
-        storageList.put(301, 600);
-
-        int widthDet = 100; //Ширина деталі
-        int lengthDet = 200; //Довжина довжина
-        int count = 0; //Сюди складемо кількість деталей, які можна виготовити зі складських запасів
-        int storage = 0; //Змінна для зберігання загальної кількості матеріалу на складі
-
+        int storage = 0; //загальна кількість на складі
+        int stockSize = 150; //Розмір заготовки
+        int count = 0; //Сюди складемо кількість заготовок, які можна заготовити зі складських запасів
 
         System.out.print("Наявні заготовки ");
-        for (Map.Entry<Integer, Integer> entry : storageList.entrySet()) {
-            int dimA = entry.getKey();
-            int dimB = entry.getValue();
-            System.out.print(dimA + "x" + dimB + " | ");
-            storage += dimA*dimB;
+        for (Integer j : aList) {
+            System.out.print(j + " | ");
+            storage += j;
         }
 
-        System.out.println("\nЗагальна кількість: " + storage + "mm2" );
+        System.out.println("\nЗагальна кількість: " + storage);
 
-        //Починається неідеальна магія
-        for (Map.Entry<Integer, Integer> entry : storageList.entrySet()) {
-            int width = entry.getKey();
-            int length = entry.getValue();
-
-            System.out.print(width + "x" + length + " - ");
-
-            if (width >= widthDet && length >= lengthDet) {
-                count += (width / widthDet) * (length / lengthDet);
-                System.out.println("ok (+" + (width / widthDet) * (length / lengthDet) + ")");
-            } else if (width >= lengthDet && length >= widthDet) {
-                count += (width / lengthDet) * (length / widthDet);
-                System.out.println("ok (+" + (width / lengthDet) * (length / widthDet) + ") - 90deg");
-            } else {
-                System.out.println("not");
-            }
+        //Починається магія
+        for (Integer j : aList) {
+            if(j>=stockSize) count+=j/stockSize;
         }
-        //Ось і все, магія звкінчилась
+        //Магія звкінчилась
 
         System.out.println("Зі складу можнна заготовити: " + count + " заготовк");
     }
