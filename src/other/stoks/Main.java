524a5391a759fd88be959acf1613c2fadb1dc54d
HizZapps <hizz.apps@gmail.com>
2024-07-10 22:12:56 +0300

    10.07.24

---
+++ src/other/stoks/Main.java
@@ -0,0 +1,37 @@
+package other.stoks;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main (String[] args){
+
+        ArrayList<Integer> aList = new ArrayList<>();
+        //Додаємо заготовки на склад
+        aList.add(70);
+        aList.add(350);
+        aList.add(50);
+        aList.add(148);
+        aList.add(449);
+        aList.add(899);
+
+        int storage = 0; //загальна кількість на складі
+        int stockSize = 150; //Розмір заготовки
+        int count = 0; //Сюди складемо кількість заготовок, які можна заготовити зі складських запасів
+
+        System.out.print("Наявні заготовки ");
+        for (Integer j : aList) {
+            System.out.print(j + " | ");
+            storage += j;
+        }
+
+        System.out.println("\nЗагальна кількість: " + storage);
+
+        //Починається магія
+        for (Integer j : aList) {
+            if(j>=stockSize) count+=j/stockSize;
+        }
+        //Магія звкінчилась
+
+        System.out.println("Зі складу можнна заготовити: " + count + " заготовк");
+    }
+}
